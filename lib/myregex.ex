62887ca49a85c032a7a419f82dd9db85d546c98e
Kazuki Shigemichi <shigemichik@gmail.com>
2018-01-05 21:53:43 +0900

    Enable to match using '?' metacharactor.
    Add matchQuestion function.

--- lib/myregex.ex
+++ lib/myregex.ex
@@ -56,23 +56,42 @@ defmodule Myregex do
     true
   end
 
-  def match(pattern , text) do
+  def match(pattern, text) do
     # workaround for testing
-    {phead, prest} = case String.next_codepoint pattern do
+    {phead, prest} = case String.next_grapheme pattern do
       nil -> {"", ""}
       t -> t
     end
-    {thead, trest} = case String.next_codepoint text do
+    {thead, trest} = case String.next_grapheme text do
       nil -> {"", ""}
       t -> t
     end
-    matchOne(phead, thead) && match(prest, trest)
+    if String.first(prest) == "?" do
+      matchQuestion(pattern, text)
+    else
+      matchOne(phead, thead) && match(prest, trest)
+    end
   end
 
   def search(pattern, text) do
-    if String.first(pattern) == "^" do
-      match(String.slice(pattern, 1..-1), text)
+    cond do
+      String.first(pattern) == "^" ->
+        match(String.slice(pattern, 1..-1), text)
+      true ->
+        l = String.length text
+        Enum.any?(
+          0..(l-1),
+          fn(i) ->
+            r = String.slice(text, i..-1)
+            match(pattern, r)
+          end
+        )
     end
   end
 
+  def matchQuestion(pattern, text) do
+    first_matched = matchOne(String.first(pattern), String.first(text))
+    matched_after_question = match(String.slice(pattern, 2..-1), String.slice(text, 1..-1))
+      (first_matched && matched_after_question) || match(String.slice(pattern, 2..-1), text)
+  end
 end
